613f57f7249e9edf6f7e9b8428c24a3328e948bd
Ohdele <adesinaayodele48@gmail.com>
2024-11-28 21:49:47 -0500

    Add eslint.config.js configuration file

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+};
